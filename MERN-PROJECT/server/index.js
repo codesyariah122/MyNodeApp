607735d456f385560ff1ffd97e96e3a344341fc6
codesyariah122 <pujiermanto@gmail.com>
2021-02-18 22:25:33 +0700

    update MERN

--- MERN-PROJECT/server/index.js
+++ MERN-PROJECT/server/index.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use('/', router)
 
 dbconf()
-
+																																																																																																									
 
 app.listen(PORT, () => {
 	console.log(`Server running on port : ${PORT}`)
--- MERN-PROJECT/server/models/postMessage.js
+++ MERN-PROJECT/server/models/postMessage.js
@@ -16,6 +16,6 @@ const postSchema = mongoose.Schema({
 	},
 });
 
-const PostMessage = mongoose.model('PostMessage', postSchema);
+const PostMessage = mongoose.model('Post.PostMessage', postSchema);
 
 export default PostMessage;
